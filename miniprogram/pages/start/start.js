bfe234971d11cd4c79ee567d903d2305fde0ca36
suyizZZ <695708197@qq.com>
2020-03-07 20:08:09 +0800

    首页修改
    首页修改

--- miniprogram/pages/start/start.js
+++ miniprogram/pages/start/start.js
@@ -2,21 +2,49 @@ const app = getApp();
 
 Page({
   data: {
-    userInfo: {
-
-    }
+    avatarUrl: './user-unlogin.png',
+    userInfo: {},
+    angle: 0,
   },
 
-    onShow:function(){
-    let that = this
-    let userInfo = wx.getStorageSync('userInfo')
-    if (!userInfo) {
-      console.log(123232);
-    } else {
-      that.setData({
-        userInfo: userInfo
-      })
-    }
+  onLoad: function () {
+    // 获取用户信息
+    wx.getSetting({
+
+      success: res => {
+
+        if (res.authSetting['scope.userInfo']) {
+
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+          wx.getUserInfo({
+
+            success: res => {
+              console.log(res, 'res');
+
+              this.setData({
+                avatarUrl: res.userInfo.avatarUrl,
+                userInfo: res.userInfo
+              });
+
+            }
+          })
+
+        }
+      }
+    })
   },
+
+  onReady: function () {
+    wx.onAccelerometerChange(function (res) {
+      var angle = -(res.x * 30).toFixed(1);
+      if (angle > 14) { angle = 14; }
+      else if (angle < -14) { angle = -14; }
+      if (that.data.angle !== angle) {
+        that.setData({
+          angle: angle
+        });
+      }
+    });
+  }
 
 })
